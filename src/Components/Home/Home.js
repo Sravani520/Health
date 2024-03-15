f79b595ec1e55b2c3571ea9f78c5d204ba4d66e3
Sravani520 <sravanichennupati222@gmail.com>
2024-03-16 02:58:30 +0530

    change in url

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -21,7 +21,7 @@ const Home = () => {
         const response = await axios.get('http://127.0.0.1:5000/sensor-data');
         setSensorData(prevData => response.data.result);
         const userId = localStorage.getItem('userId');
-        await axios.post(`https://healthguard-backend-sensordatasave.onrender.com/sensor-data/${userId}`, {
+        await axios.post('https://healthguard-backend-sensordatasave.onrender.com/sensor-data', {
         temperature: sensorData.t,
         heartbeat: sensorData.h,
         spo2: sensorData.s,
